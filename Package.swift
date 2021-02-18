61076058cf4c728b93f63b55bfdb615b4b4c704e
Patrik Svoboda <svob.pata@gmail.com>
2021-02-18 16:11:37 +0100

    WatchOS support

--- Package.swift
+++ Package.swift
@@ -1,4 +1,4 @@
-// swift-tools-version:5.0
+// swift-tools-version:5.3
 // The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
@@ -7,7 +7,9 @@ let package = Package(
     name: "Peep",
     platforms: [
         .iOS(.v10),
-        .tvOS(.v12)
+        .tvOS(.v12),
+        .watchOS(.v7),
+        .macOS(.v11)
     ],
     products: [
         // Products define the executables and libraries produced by a package, and make them visible to other packages.
